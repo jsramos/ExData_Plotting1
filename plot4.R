8c137232674af69a4946fd6d45f4daecca8fbbbe
ramos.cardona@gmail.com <ramos.cardona@gmail.com>
2015-04-08 19:56:34 -0500

    Uploaded R script and PNG for plot 4.

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
-# Plot3.R
+# Plot4.R
 # This script creates the plot as it appears on 
-# https://github.com/rdpeng/ExData_Plotting1/blob/master/figure/unnamed-chunk-4.png
+# https://github.com/jsramos/ExData_Plotting1/blob/master/figure/unnamed-chunk-5.png
 # The plot is created as part of Course Project 1 for the 'Exploratory Data Analysis' Coursera MOOC.
 
 # Execute common code for all plots in file 'prepare_data.R'
@@ -14,15 +14,18 @@ if (file.exists("prepare_data.R")) {
 .pardefault <- par(no.readonly = T)
 
 # Create 1st Plot on PNG graphic device
+png("plot4.png", width = 480, height = 480, bg = "transparent")
 par(mar=c(4,4,2,2))
-png("plot3.png", width = 480, height = 480, bg = "transparent")
+par(mfrow=c(2,2))
 with(pcdata, {
+        plot(Global_active_power ~ timestamp, type = "l", xlab="", ylab="Global Active Power")
+        plot(Voltage ~ timestamp, type = "l", xlab="datetime", ylab="Voltage")
         plot(Sub_metering_1 ~ timestamp, type = "l", xlab="", ylab="Energy sub metering")
         lines(Sub_metering_2 ~ timestamp, col = "red")
         lines(Sub_metering_3 ~ timestamp, col = "blue")
-        legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1,1))
-        })
-
+        legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1,1), bty="n", pt.cex = 1, cex = 0.8)
+        plot(Global_reactive_power ~ timestamp, type = "l", xlab="datetime")
+})
 
 # Close the graphics device
 dev.off()
